cd0eee2f1536b56b2271cd007c976d03d8b9b7ee
Ali Mert Yilmaz <64384082+alimert1@users.noreply.github.com>
2022-10-02 01:31:28 +0300

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyparser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+
+const app = express();
+
+app.use(bodyParser.json({limit:"30mb" , extended : true}));
+app.use(bodyParser.json({limit:"30mb" , extended : true}));
+app.use(cors());
+
+app.get("/" , (req,res) =>{
+    res.send("FullstackBlog.......");
+});
+
+const PORT = process.env.PORT || 5000;
+
+const CONNECTION_URL = "mongodb+srv://alimert:<password>@cluster0.w1jazfn.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose
+.connect(CONNECTION_URL , {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(() =>{
+    app.listen(PORT , () =>{
+        console.log("Server is running on porttt:")
+    })
+})
